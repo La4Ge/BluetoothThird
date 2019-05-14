1977d9725e9a450d20f4dbde974538f8d194c834
Lars <lars.gebert@it-designers.de>
2019-05-14 15:05:20 +0200

    Bluetooth working, needs sound implementation lul

--- app/src/main/java/com/example/bluetooththird/MainActivity.java
+++ app/src/main/java/com/example/bluetooththird/MainActivity.java
@@ -25,7 +25,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -44,16 +43,13 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button send;
     ListView listView;
-    TextView msg_box;
 
     BluetoothAdapter bluetoothAdapter;
     BluetoothDevice[] btArray;
 
     SendReceive sendReceive;
 
-    Date currentTime;
     static final int STATE_LISTENING = 1;
     static final int STATE_CONNECTING = 2;
     static final int STATE_CONNECTED = 3;
@@ -62,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
     static int REQUEST_ENABLE_BLUETOOTH=1;
 
-    public String messageFromBluetooth;
     private static final String APP_NAME = "BTChat";
     private static final UUID MY_UUID = UUID.fromString("8ce255c0-224a-11e0-ac64-0803450c9a66");
     Context context;
@@ -72,6 +67,50 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
+    private static final int SAMPLING_RATE_IN_HZ = 44100;
+
+    private static final int REQUEST_BABY = 7;
+
+    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+
+    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+
+    private static final int BUFFER_SIZE_FACTOR = 2;
+
+    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLING_RATE_IN_HZ,
+            CHANNEL_CONFIG, AUDIO_FORMAT) * BUFFER_SIZE_FACTOR;
+
+    private final AtomicBoolean recordingInProgress = new AtomicBoolean(false);
+
+    private AudioRecord recorder = null;
+
+    private Thread recordingThread = null;
+
+    private Button startButton;
+
+    double difference_calculated = 0;
+
+    boolean delaythemedia = true;
+
+    private Button stopButton;
+
+    String tempCalcResult = "";
+
+    private Button detectButton;
+
+    byte[] bytes = new byte[10000000];
+
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+    long byte_size = 0;
+
+    File file2 = new File("/storage/emulated/0/zirp.wav");
+    File file1 = new File("/storage/emulated/0/recording.pcm");
+
+
+    double[] result_snipped = SoundDataUtils.load16BitPCMRawDataFileAsDoubleArray(file2);
 
 
 
@@ -84,6 +123,8 @@ public class MainActivity extends AppCompatActivity {
         if(extras !=null)
         {
             if(extras.getString("message")!= null){
+                Bundle bun= savedInstanceState;
+                bluetoothAdapter= BluetoothAdapter.getDefaultAdapter();
                 String msg=extras.getString("message");
                 sendReceive.write(msg.getBytes());
                 finish();
@@ -118,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 
     public void requestDiscover()
     {
@@ -307,9 +349,8 @@ public class MainActivity extends AppCompatActivity {
                     text = "Verbunden";
                     toast = Toast.makeText(context, text, Toast.LENGTH_LONG);
                     toast.show();
-
-                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                    startActivity(intent);
+                    setContentView(R.layout.second_view);
+                    impelemntSecondView();
                     break;
                 case STATE_CONNECTION_FAILED:
                     text = "Verbindung fehlgeschlagen, versuchen Sie es mit einem Neustart!";
@@ -320,9 +361,9 @@ public class MainActivity extends AppCompatActivity {
                 case STATE_MESSAGE_RECEIVED:
                     byte[] readBuffer= (byte[]) msg.obj;
                     String tempMsg= new String(readBuffer, 0, msg.arg1);
-                    Intent intent3 = new Intent(MainActivity.this, SecondActivity.class);
-                    intent3.putExtra("getMessage", tempMsg);
-                    startActivity(intent3);
+                    sometry(tempMsg);
+
+
 
                     break;
             }
@@ -505,19 +546,336 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    public void impelemntSecondView(){
+        requestAllPermissions();
+
+        startButton = (Button) findViewById(R.id.btnStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+
+                try {
+                    String string = "start";
+                    sendReceive.write(string.getBytes());
+
+                }catch (Exception e){
+
+                }
+
+                startRecording();
+                }
+        });
+
+        stopButton = (Button) findViewById(R.id.btnStop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                delaythemedia = true;
+
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
 
 
 
+                try {
+                    String string = "stop";
+                    sendReceive.write(string.getBytes());
+                }catch (Exception e){
+
+                }
+
+                stopRecording();
+            }
+        });
+
+        detectButton = (Button) findViewById(R.id.btnDetect);
+        detectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detect();
+            }
+        });
 
 
 
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+    }
 
 
 
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    int PERMISSION_ALL = 1;
+
+    String[] PERMISSIONS = {
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.RECORD_AUDIO
+    };
+
+
+    public void requestAllPermissions() {
+        if (!hasPermissions(this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+
+/*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startButton.setEnabled(true);
+        stopButton.setEnabled(false);
+    }
+*/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopRecording();
+    }
+
+
+    private void startRecording() {
+        recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLING_RATE_IN_HZ,
+                CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
+
+        recorder.startRecording();
+
+        difference_calculated = 0;
+        recordingInProgress.set(true);
+
+        try {
+            baos.reset();
+        } catch (Exception e) {
+        }
+
+
+        recordingThread = new Thread(new RecordingRunnable(), "Recording Thread");
+        recordingThread.start();
+
+/*
+        if (delaythemedia == true) {
+
+
+            try {
+                recordingThread.sleep(1000);
+                final MediaPlayer mo = MediaPlayer.create(this, R.raw.zirp);
+                mo.start();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        } else {
+
+            try {
+                recordingThread.sleep(300);
+                final MediaPlayer mo = MediaPlayer.create(this, R.raw.zirp);
+                mo.start();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    */
+    }
 
 
 
+    private void stopRecording() {
+        if (null == recorder) {
+            return;
+        }
 
+        recordingInProgress.set(false);
+
+        recorder.stop();
+
+        recorder.release();
+
+        recorder = null;
+
+        recordingThread = null;
+
+
+        detect();
+
+
+        try {
+            recordingThread.sleep(300);
+
+            try {
+                String tempMsg = String.valueOf(difference_calculated);
+                sendReceive.write(tempMsg.getBytes());
+
+            }catch (Exception e){
+            }
+
+
+
+        } catch (InterruptedException e) {
+        }
+
+
+        //distance(difference);
+    }
+
+
+    private double detect() {
+        //File file = new File("/storage/emulated/0/recording.pcm");
+        //double[] result = SoundDataUtils.load16BitPCMRawDataFileAsDoubleArray(file);
+        //bytes[1] = 0;
+
+
+        //ByteArrayInputStream inStream = new ByteArrayInputStream(baos.toByteArray());
+        //double[] result = new double[baos.size()];
+//
+        //try {
+        //    result = SoundDataUtils.readStreamAsDoubleArray(inStream, baos.size());
+        //} catch (Exception e) {
+        //}
+
+        double[] result = SoundDataUtils.load16BitPCMRawDataFileAsDoubleArray(file1);
+
+        difference_calculated = Detect.isit(result, result_snipped);
+
+        return difference_calculated;
+    }
+
+
+    private void distance(double difference_received) {
+
+        while(difference_calculated == 0){
+            distance(difference_received);
+        }
+        double difference_all = Math.abs(difference_received - difference_calculated);
+        double constantDistance = 0.00;
+        double speedOfSound = 343.2;
+
+        double endergebnis = ((difference_all) / 44100) * speedOfSound / 2 + constantDistance;
+
+        if(endergebnis > 15.0){
+            endergebnis = 15.0;
+        }else if(endergebnis< 0){
+            endergebnis = 0;
+        }
+
+        final TextView textViewToChange = (TextView) findViewById(R.id.textViewCorr);
+
+        String newtext = Double.toString(endergebnis);
+
+        textViewToChange.setText(newtext);
+    }
+
+    private class RecordingRunnable implements Runnable {
+
+        @Override
+        public void run() {
+
+            final File file = new File(Environment.getExternalStorageDirectory(), "recording.pcm");
+            final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+
+            try (final FileOutputStream outStream = new FileOutputStream(file)) {
+                while (recordingInProgress.get()) {
+                    int result = recorder.read(buffer, BUFFER_SIZE);
+                    if (result < 0) {
+                        throw new RuntimeException("Reading of audio buffer failed: " +
+                                getBufferReadFailureReason(result));
+                    }
+                    outStream.write(buffer.array(), 0, BUFFER_SIZE);
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Writing of recorded audio failed", e);
+            }
+        }
+        /*
+
+            //final File file = new File(Environment.getExternalStorageDirectory(), "recording.pcm");
+            final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+
+
+            //try (final FileOutputStream outStream = new FileOutputStream(file)) {
+
+
+            while (recordingInProgress.get()) {
+                int result = recorder.read(buffer, BUFFER_SIZE);
+                if (result < 0) {
+                    throw new RuntimeException("Reading of audio buffer failed: " +
+                            getBufferReadFailureReason(result));
+                }
+                //dooooooooomed
+                //outStream.write(buffer.array(), 0, BUFFER_SIZE);
+                //buffer.array().get(bytes,0,BUFFER_SIZE);
+                baos.write(buffer.array(), 0, BUFFER_SIZE);
+                //byte_size++;
+                buffer.clear();
+            }
+
+
+            //} catch (IOException e) {
+            //throw new RuntimeException("Writing of recorded audio failed", e);
+            //}
+        }*/
+
+        private String getBufferReadFailureReason(int errorCode) {
+            switch (errorCode) {
+                case AudioRecord.ERROR_INVALID_OPERATION:
+                    return "ERROR_INVALID_OPERATION";
+                case AudioRecord.ERROR_BAD_VALUE:
+                    return "ERROR_BAD_VALUE";
+                case AudioRecord.ERROR_DEAD_OBJECT:
+                    return "ERROR_DEAD_OBJECT";
+                case AudioRecord.ERROR:
+                    return "ERROR";
+                default:
+                    return "Unknown (" + errorCode + ")";
+            }
+        }
+    }
+
+    private void sometry(String temp) {
+
+
+        switch (temp) {
+            case "start":
+                //difference_calculated = 0;
+                delaythemedia = false;
+
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+
+                startRecording();
+                break;
+            case "stop":
+                delaythemedia = true;
+                startButton.setEnabled(true);
+                stopButton.setEnabled(false);
+
+                stopRecording();
+                break;
+            case "calc":
+                //distance();
+                break;
+            default:
+                double difference = Double.parseDouble(temp);
+                distance(difference);
+                break;
+        }
+
+
+    }
 
 
 
